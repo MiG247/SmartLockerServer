81d94a1c92bcae94e352b54e93c4377b40da24ba
Michael Kl <michaelk247@yahoo.de>
2017-10-05 13:09:06 +0800

    made some test changes

--- controllers/Smartlocker.js
+++ controllers/Smartlocker.js
@@ -6,5 +6,5 @@ var Smartlocker = require('./SmartlockerService');
 
 
 module.exports.getHtml = function getHtml (req, res, next) {
-  Smartlocker.getHtml(req, res, next);
+  Smartlocker.getHtml(req.swagger.params, res, next);
 };
