b3edb313929d930268b56434733b78a7b82e0f46
Michael Kl <michaelk247@yahoo.de>
2017-10-18 15:11:39 +0800

    fixed creates sql file

--- Documentation/DatabaseSQL_Creates.sql
+++ Documentation/DatabaseSQL_Creates.sql
@@ -4,31 +4,15 @@ create table combo(
 	id int(10) not null unique,
 	name char(255) not null unique,
 	price float(10) not null,
-	photo BLOB(),
+	photo BLOB,
 	primary key(id)
-);
-
-create table food_combo(
-	food_id int (10) not null,
-	combo_id int (10) not null,
-	primary key(food_id, combo_id),
-	foreign key(food_id) references food,
-	foreign key(combo_id) references combo
 );
 
 create table food(
 	id int(10) not null unique,
 	name char(255) not null unique,
-	photo BLOB(),
+	photo BLOB,
 	primary key(id)
-);
-
-create table food_ingredient(
-	food_id int(10) not null,
-	ingredient_id int(10) not null,
-	primary key(food_id, ingredient_id),
-	foreign key(food_id) references food,
-	foreign key(ingredient_id) references ingredient
 );
 
 create table ingredient(
@@ -37,17 +21,9 @@ create table ingredient(
 	primary key(id)
 );
 
-create table orders(
-	id int(10) not null unique,
-	combo_id int(10) not null,
-	datestamp date not null, -- not in ERM but might be usefull for a orders
-	primary key(id),
-	foreign key(combo_id) references combo
-);
-
 create table schedule(
-	pickup_time time(7) not null unique,
-	available boolean(1),
+	pickup_time time(0) not null unique, -- should be time, using int got to convert hh.mm
+	available boolean,
 	primary key(pickup_time)
 );
 
@@ -57,12 +33,37 @@ create table locker(
 	primary key(nr)
 );
 
+create table orders(
+	id int(10) not null unique auto_increment,
+	combo_id int(10) not null,
+	datestamp date not null, -- not in ERM but might be usefull for a orders
+	primary key(id),
+	foreign key(combo_id) references combo (id)
+);
+
 create table locker_schedule(
-	pickup_time time(7) not null,
+	pickup_time time(0) not null, -- should be time, got to convert it hh:mm
 	locker_nr int(10) not null,
 	orders_id int(10) not null,
 	primary key(pickup_time, locker_nr, orders_id),
-	foreign	key(pickup_time) references schedule,
-	foreign key (locker_nr) references locker,
-	foreign key(orders_id) references orders
+	foreign	key(pickup_time) references schedule (pickup_time),
+	foreign key (locker_nr) references locker (nr),
+	foreign key(orders_id) references orders (id)
+);
+
+create table food_combo(
+	food_id int (10) not null,
+	combo_id int (10) not null,
+	primary key(food_id, combo_id),
+	foreign key(food_id) references food (id),
+	foreign key(combo_id) references combo (id)
+);
+
+
+create table food_ingredient(
+	food_id int(10) not null,
+	ingredient_id int(10) not null,
+	primary key(food_id, ingredient_id),
+	foreign key(food_id) references food (id),
+	foreign key(ingredient_id) references ingredient (id)
 );
