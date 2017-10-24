7bb462d40a3b0c6a152fc79da51649f0455de937
Michael Kl <michaelk247@yahoo.de>
2017-10-24 17:12:19 +0800

    mysql connection to dokker mysql

--- MySQL.js
+++ MySQL.js
@@ -3,13 +3,20 @@
 var mysql = require('mysql');
 
 //Connection Variables needs to be adjusted !!!
+/*
 var mysql_db = mysql.createConnection({
   host: 'localhost',
   user: 'maik',
   password: 'test123',
   database: 'smartlocker_db'
 });
-
+*/
+var mysql_db = mysql.createConnection({
+  host: 'localhost',
+  user: 'mysql',
+  password: '',
+  database: 'smartlocker_db'
+});
 // Connect to DB
 mysql_db.connect((err) => {
     if(err){
