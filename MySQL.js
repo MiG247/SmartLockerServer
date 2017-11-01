38423f4ba9a3272ca31bf6cf8d0d4bdf430a998b
Michael Kl <michaelk247@yahoo.de>
2017-11-01 15:25:56 +0800

    changed to dokku connect

--- MySQL.js
+++ MySQL.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mysql = require('mysql');
-
+/*
 //Connection Variables
 var mysql_db = mysql.createConnection({
   host: 'localhost',
@@ -10,7 +10,8 @@ var mysql_db = mysql.createConnection({
   database: 'smartlocker_db',
   multipleStatements: true
 });
-/*
+*/
+///*
 var mysql_db = mysql.createConnection({
   host: 'dokku-mysql-smartlocker-db',
   port: 3306,
@@ -19,7 +20,7 @@ var mysql_db = mysql.createConnection({
   database: 'smartlocker_db',
   multipleStatements: true
 });
-*/
+//*/
 //connect to the DB
 mysql_db.connect((err) => {
       if(err){
